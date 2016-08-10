2cd883df7f0319223944da5ce129cded8b552196
Egor <vitkovskynet@yandex.ru>
2016-08-10 22:33:01 +0300

    misc

--- ShopPrototype/ShopPrototype/Controllers/HomeController.cs
+++ ShopPrototype/ShopPrototype/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace ShopPrototype.Controllers
 			return View();
 		}
 
+		public ActionResult Search()
+		{
+			return View();
+		}
+
 		public ActionResult About()
 		{
 			ViewBag.Message = "Your application description page.";
