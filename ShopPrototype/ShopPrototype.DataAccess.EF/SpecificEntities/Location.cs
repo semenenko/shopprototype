72e949ac6ede7683b9a58034eb97662fbda9979f
Egor <vitkovskynet@yandex.ru>
2016-08-20 22:01:02 +0300

    packages update

---
+++ ShopPrototype/ShopPrototype.DataAccess.EF/SpecificEntities/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Spatial;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopPrototype.DataAccess.EF.SpecificEntities
+{
+	public class Location
+	{
+		public int Id { get; set; }
+
+		public DbGeography Geography { get; set; }
+	}
+}
