86987948c65501b1d3eba42d980074344f9372fa
Egor <vitkovskynet@yandex.ru>
2016-08-30 22:43:47 +0300

    angular study

--- ShopPrototype/ShopPrototype.Front.Angular/Scripts/app.js
+++ ShopPrototype/ShopPrototype.Front.Angular/Scripts/app.js
@@ -17,34 +17,11 @@
 //	];
 //});
 
-angular.module('phonecatApp').
-	component('greetUser', {
-		template: 'Hello, {{$ctrl.user}}!',
-		controller: function GreetUserController() {
-			this.user = 'world';
-		}
-	});
+//angular.module('phonecatApp').
+//	component('greetUser', {
+//		template: 'Hello, {{$ctrl.user}}!',
+//		controller: function GreetUserController() {
+//			this.user = 'world';
+//		}
+//	});
 
-angular.module('phonecatApp').
-	component('phoneList', {
-		template:'<ul>' + 
-			'<li ng-repeat="phone in $ctrl.phones">' +
-			'<span>{{phone.name}}</span>' + 
-			'<p>{{phone.snippet}}</p>' +
-			'</li>' + 
-			'</ul>',
-		controller: function PhoneListController() {
-			this.phones = [
-			{
-        		name: 'Nexus S',
-        		snippet: 'Fast just got faster with Nexus S.'
-			}, {
-        		name: 'Motorola XOOM™ with Wi-Fi',
-        		snippet: 'The Next, Next Generation tablet.'
-			}, {
-        		name: 'MOTOROLA XOOM™',
-        		snippet: 'The Next, Next Generation tablet.'
-			}
-			];
-		}
-	});
---
+++ ShopPrototype/ShopPrototype.Front.Angular/Scripts/phone-list/phone-list.component.js
@@ -0,0 +1,25 @@
+﻿angular.module('phonecatApp').
+	component('phoneList', {
+		templateUrl: '/scripts/phone-list/phone-list.template.html',
+		controller: function PhoneListController() {
+			this.phones = [
+			{
+				name: 'Nexus S',
+				snippet: 'Fast just got faster with Nexus S.',
+				age: 1
+			}, 
+			{
+				name: 'Motorola XOOM™ with Wi-Fi',
+				snippet: 'The Next, Next Generation tablet.',
+				age: 2
+			}, 
+			{
+				name: 'MOTOROLA XOOM™',
+				snippet: 'The Next, Next Generation tablet.',
+				age: 3
+			}
+			];
+
+			this.orderBy = 'age';
+		}
+	});
